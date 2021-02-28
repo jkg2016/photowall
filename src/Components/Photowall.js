5201e63c31fb4b1cec6c47e2bdd3cf96bcc7dce3
Jayman Gandhi <jayman.gandhi@gmail.com>
2021-02-28 13:37:11 -0500

    Basic React Code

---
+++ src/Components/Photowall.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Photo from "./Photo";
+import PropTypes from 'prop-types';
+
+export default function Photowall(props) {
+  return (
+    <div>
+      <div className="photoGrid">
+        {props.posts.map((post, index) => (
+          <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+Photowall.propTypes = {
+  posts: PropTypes.array.isRequired,
+  onRemovePhoto: PropTypes.func.isRequired
+}
---
+++ src/Components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function Title(props) {
+  return (
+    <>
+      return <h1> {props.title} </h1>
+    </>
+  )
+}
+
