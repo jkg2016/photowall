298d3a3134d3fc837ed71a2414dea259ef217c6b
Jayman Gandhi <jayman.gandhi@gmail.com>
2021-04-12 15:14:19 -0400

    Converting to Firbase

---
+++ src/database/config.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyBZZXuAmCyc4V9pMDFZc2dkjxHqcYWIgRQ",
+  authDomain: "photowall-fb3f8.firebaseapp.com",
+  databaseURL: "https://photowall-fb3f8-default-rtdb.firebaseio.com",
+  projectId: "photowall-fb3f8",
+  storageBucket: "photowall-fb3f8.appspot.com",
+  messagingSenderId: "699293903271",
+  appId: "1:699293903271:web:fa2896c66e25e16745ae82",
+};
+
+firebase.initializeApp(firebaseConfig);
